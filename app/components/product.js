8e4197fc803260a3a5f84eec81b81b3af1227bf4
Shane Garland <shanegarland@Shanes-MacBook-Pro.local>
2022-04-20 16:29:15 +1000

    complete video 5

---
+++ app/components/product.js
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/actions';
+
+export default class ProductComponent extends Component {}
